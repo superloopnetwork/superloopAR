1e624d0886f1ff976e1a7925d490e34146512d20
Wai Lit Loi <wailit@gmail.com>
2017-09-12 22:07:33 +0000

    It will now only read new log entries and not the last log line

--- main.py
+++ main.py
@@ -6,19 +6,23 @@ import re
 def main():
 
 	syslog = []
-	index = 0
 
 	tail = subprocess.Popen('tail -f -n 1 /mnt/syslog/**/*.log', shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-
+	
 	while True:
 		line = tail.stdout.readline()
 		line = line.strip('\n')
 		if(re.search('mnt',line)) or (line == ''):
+			purge = True
 			pass	
 		else:
-			syslog.append(line)
-			multithread_engine(syslog)
-			del syslog[:]
+			if(purge):
+				purge = False
+				pass
+			else:
+				syslog.append(line)
+				multithread_engine(syslog)
+				del syslog[:]
 
 if __name__ == '__main__':
 	main()
