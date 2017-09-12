b47ba70441488db780b9285c041dbf228cd41ccc
Wai Lit Loi <wailit@gmail.com>
2017-09-12 22:10:46 +0000

    Unstructed to structured data using regular expressions to parse

--- parser.py
+++ parser.py
@@ -1,3 +1,25 @@
+####################### ENGINE FUNCTIONS ########################
+import re
 
 def parser(syslog_line):
-	print syslog_line
+
+	DATESTAMP_RE = r'(.+?\s+\d+)'
+	TIMESTAMP_RE = r'(\d+:\d+:\d+)'
+	DEVICE_IP_RE = r'(\d+\.\d+\.\d+\.\d+)'
+	ERROR_CODE_RE = r':\s%*(.+?):'
+	ERROR_MESSAGE_RE = r'%.+:\s(.*)'
+
+	dstamp = re.search(DATESTAMP_RE, syslog_line).group(1)
+	tstamp = re.search(TIMESTAMP_RE, syslog_line).group(1)
+	device = re.search(DEVICE_IP_RE, syslog_line).group(1)
+	error_c = re.search(ERROR_CODE_RE, syslog_line).group(1)
+	error_m = re.search(ERROR_MESSAGE_RE, syslog_line).group(1)
+
+	f = open('devices')
+	list = f.readlines()
+
+	for element in list:
+		element = element.strip('\n')
+		device_mapping = element.split(',')
+
+	print dstamp, tstamp, device, error_c, error_m
