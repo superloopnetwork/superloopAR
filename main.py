7d2b34d2b4c00aeb4acd0f37d29fb2a4ae34b5cd
Wai Lit Loi <wailit@gmail.com>
2017-09-14 17:25:27 +0000

    Algorithm change in how it reads the tail

--- main.py
+++ main.py
@@ -8,21 +8,34 @@ def main():
 	syslog = []
 
 	tail = subprocess.Popen('tail -f -n 1 /mnt/syslog/**/*.log', shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	database = []
 	
 	while True:
 		line = tail.stdout.readline()
 		line = line.strip('\n')
-		if(re.search('mnt',line)) or (line == ''):
-			purge = True
-			pass	
-		else:
-			if(purge):
+		
+		if(re.search('mnt',line)): 
+			if(not database):
+				database.append(line)
+				purge = True
+			elif(line in database):
+				print 'IN DATABASE: %s' % line
 				purge = False
-				pass
 			else:
-				syslog.append(line)
-				multithread_engine(syslog)
-				del syslog[:]
+				database.append(line)
+				purge = True
+			
+		elif(line == ''):
+			pass
+		
+		elif(purge):
+			purge = False
+			pass
+		
+		else:
+			syslog.append(line)
+			multithread_engine(syslog)
+			del syslog[:]
 
 if __name__ == '__main__':
 	main()
