2cda4b19314f6dfb1224446a889272423c39e93c
Wai Lit Loi <wailit@gmail.com>
2017-09-16 22:55:24 +0000

    Updates to re-tailing. Re-tailing occurs only when new devices are added

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def main():
 	while True:
 
 		tail = subprocess.Popen('tail -f -n 0 /mnt/syslog/**/*.log', shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+		before_count = int(ls())
 		refresh = True
 		
 		while refresh:
@@ -24,8 +25,22 @@ def main():
 				syslog.append(line)
 				multithread_engine(syslog)
 				del syslog[:]
-				tail.kill()
-				refresh = False
+				current_count = int(ls())
+				print 'Before Count: %s' % before_count
+				print 'Current Count: %s' % current_count
+				if(current_count > before_count):
+					tail.kill()
+					refresh = False
+
+def ls():
+
+	ls = subprocess.Popen('ls /mnt/syslog/**/*.log | wc -l', shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	list = ls.stdout.readline()
+	list = list.strip('\n')
+	devcount = int(list)
+	ls.kill()
+
+	return devcount
 
 if __name__ == '__main__':
 	main()
