f9ffd13648eb06b519c9bdc9bbec081f2c1c0615
Wai Lit Loi <wailit@gmail.com>
2017-12-26 19:31:47 +0000

    Added comments to help with understanding of development

--- remediation.py
+++ remediation.py
@@ -4,8 +4,11 @@ from mapping import get_remediation
 
 def remediation(dstamp,tstamp,ip_address,error_code,error_message,ntw_device):
 
+	# MODULE VARIABLE CALLS THE GET_REMEDIATION FUNCTION AND RETURNS THE SPECIFIC REMEDIATION REQUIRED TO BE USED WITHIN THE LIB/REMEDIATION DIRECTORY
 	module = get_remediation(error_code)
 
+	# DEVICE CREATES AN OBJECT OF THE DEVICE USING NTW_DEVICE(LIST) FROM THE ABOVE MODULE VARIABLE(CLASS)
 	device = module(*ntw_device)
 
+	# CALLING THE REMEDIATE METHOD WITHIN THE SPECIFIED CLASS
 	device.remediate(dstamp,tstamp,ip_address,error_code,error_message)
