8966fc13725d54089260caeebe98ebccea967cc5
Wai Lit Loi <wailit@gmail.com>
2018-07-18 03:48:02 +0000

    Added some comments to help walkthrough

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from parser import parser
 from multithread import multithread_engine
 import lib.globalvar
@@ -32,19 +34,31 @@ def main():
 				tail.kill()
 				purge = False
 				refresh = False
-			
+
+			### IF THE OUTPUT FROM THE TAIL HAS 'mnt' OR '', DO NOTHING (DON'T PROCESS)	
 			if(re.search('mnt',line) or line == ''): 
 				pass
-			
+	
+			### START THE PROCESSING OF THE LOG LINES 
 			else:
+
+				### IF THE LINE ALREADY EXISTS IN HISTORY, DO NOTHING (DON'T PROCESS). THIS IS TO ACCOMODATE ANY RE-TAILING FROM HAPPENING.
 				if(line in history):
 					pass
 				else:
+
+					### APPEND THE OUTPUT LOG ENTRY ONTO THE SYSLOG LIST VARIABLE
 					syslog.append(line)
+
+					### HISTORY LIST ACTS AS A BUFFER. MAX SIZE = 1000
 					if(len(history)<=1000):
 						history.append(line)
+
+					### WHEN THE LENGTH OF HISTORY EXCEEDS 1000, POP OFF THE FIRST ENTRY
 					elif(len(history)>1000):
 						history.pop(0)
+
+					### START THE PROCESSING OF SYSTEM VIA MULTITHREAD
 					multithread_engine(syslog)
 					del syslog[:]
 
