416c0a6d4ec88dabd0b0c1e31e97248f2b031d7f
Wai Lit Loi <wailit@gmail.com>
2022-10-21 10:30:41 -0400

    Updated REs

--- parser.py
+++ parser.py
@@ -4,12 +4,13 @@ import re
 
 def parser(syslog_line):
 
+	INTERFACE_RE = r'(\w+\d+\/\d+)'
 	DATESTAMP_RE = r'(.+?\s+\d+)'
 	TIMESTAMP_RE = r'(\d+:\d+:\d+)'
 	DEVICE_IP_RE = r'(\d+\.\d+\.\d+\.\d+)'
-	ERROR_CODE_RE = r':\s%*(.+?):'
+	ERROR_CODE_RE = r'%([^:]+):'
 	ERROR_MESSAGE_RE = r'%.+:\s(.*)'
-
+	
 	dstamp = re.search(DATESTAMP_RE, syslog_line).group(1)
 	tstamp = re.search(TIMESTAMP_RE, syslog_line).group(1)
 	device = re.search(DEVICE_IP_RE, syslog_line).group(1)
